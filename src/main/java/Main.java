6e651f5edbefbe9f923683373e7abdef20d5d732
istvan-tamas <istvan.tamas90@gmail.com>
2025-06-15 12:05:31 +0200

    Added DB object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
-import DB.DatabaseConfig;
+import DB.DB;
+
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+            try (var connection =  DB.connect()){
+                System.out.println("Connected to the PostgreSQL database.");
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+
     }
 }
